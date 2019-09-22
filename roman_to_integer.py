9d6f0cc19bf8c8d118914683a7e15ad7e3bd8d7d
il.tranqui <44050420+FullPitMetal@users.noreply.github.com>
2019-09-22 19:08:18 +0200

    Add files via upload

---
+++ roman_to_integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(s: str) -> int:
+        int = 0
+        l = list(s)
+        print(type(int))
+        char = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
+        for i in len(l):
+            if char[l[i]] < char[l[i+1]]:
+                int = int - char[l[i]]
+            elif char[l[i]] >= char[l[i+1]]:
+                int = int + char[l[i]]
+    str = "M"
+    print(romanToInt(str))   
